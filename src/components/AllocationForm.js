f8d6175a039b1b700db87b2c5cb1afb88313d274
Algy Jr <algymussa@gmail.com>
2023-01-15 18:37:07 -0500

    feat: make assignment changes for submission

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,19 +2,18 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch, remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
 
     const submitEvent = () => {
-
-            if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
-                setCost("");
-                return;
-            }
+        if(cost > remaining) {
+            alert(`The value cannot exceed remaining funds  ${currency}remaining`);
+            setCost("");
+            return;
+        }
 
         const expense = {
             name: name,
@@ -26,11 +25,11 @@ const AllocationForm = (props) => {
                 payload: expense,
             });
         } else {
-                dispatch({
-                    type: 'ADD_EXPENSE',
-                    payload: expense,
-                });
-            }
+            dispatch({
+                type: 'ADD_EXPENSE',
+                payload: expense,
+            });
+        }
     };
 
     return (
@@ -58,14 +57,16 @@ const AllocationForm = (props) => {
                         <option value="Reduce" name="Reduce">Reduce</option>
                     </select>
 
+                    <span>{currency}
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
                         style={{ marginLeft: '2rem' , size: 10}}
+                        pattern='ˆ[0-9]+$'
                         onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                        </input></span>
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>Save</button>
                 </div>
             </div>
